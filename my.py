3e70bc4862bcbf3ea4492fabfc616765c6f33be2
tech-in-tech <103058497+tech-in-tech@users.noreply.github.com>
2023-05-11 23:14:48 +0530

    Add files via upload

---
+++ my.py
@@ -0,0 +1,25 @@
+# Q1. What is a set in Python?
+# a set is an unorderedd collection of uniqiue element.It is a built in data type which is used to store a multiple items
+
+
+# Q2.How do you create a set in Python?
+s = {5, 6, 7, 8, 9}
+print(s)
+
+
+# Q3. How do you add elements to a set in Python?
+s2 = {1, 2, 3}
+s2.add(4)
+print(s2)
+
+
+# Q4. How do you remove elements from a set in Python?
+s3 = {1, 2, 3, 4, 5}
+s3.remove(3)
+print(s3)
+
+
+# Q5. How do you get the length of a set in Python?
+s4 = {1, 2, 3, 4, 5}
+length = len(s4)
+print(length)
